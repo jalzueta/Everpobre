31925798261c85fe1e2bdf1b70e44ac87b49c5a9
Javi Alzueta <jalzuetap@gmail.com>
2015-04-08 21:05:58 +0200

    propiedad cosmetica (transient) a FLGPhoto

--- Everpobre/FLGPhoto.h
+++ Everpobre/FLGPhoto.h
@@ -1,5 +1,12 @@
+
+@import UIKit;
+
 #import "_FLGPhoto.h"
 
 @interface FLGPhoto : _FLGPhoto {}
 // Custom logic goes here.
+
+// Propiedad "transient" por código: esta no va a Core Data
+@property (strong, nonatomic) UIImage *image;
+
 @end
