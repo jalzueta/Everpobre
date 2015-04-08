5cdb0cab8657dfa4b81157e0ae4a66445b6aa879
Javi Alzueta <jalzuetap@gmail.com>
2015-04-09 00:24:29 +0200

    añadir notebooks

--- Everpobre/FLGNotebooksViewController.m
+++ Everpobre/FLGNotebooksViewController.m
@@ -20,6 +20,13 @@
     self.title = @"Everpobre";
 }
 
+- (void) viewWillAppear:(BOOL)animated{
+    [super viewWillAppear:animated];
+    
+    [self addNewNotebookButton];
+    [self addEditNotebookButton];
+}
+
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     
     // Averiguar cual es la libreta
@@ -40,5 +47,41 @@
     //Devolverla
     return cell;
 }
+
+
+#pragma mark - Utils
+
+- (void) addNewNotebookButton{
+    
+    UIBarButtonItem *addBtnItem = [[UIBarButtonItem alloc]
+                                   initWithBarButtonSystemItem:UIBarButtonSystemItemAdd
+                                   target:self
+                                   action:@selector(addNewNotebook:)];
+    
+    self.navigationItem.rightBarButtonItem = addBtnItem;
+}
+
+- (void) addEditNotebookButton{
+    
+//    UIBarButtonItem *editBtnItem = [[UIBarButtonItem alloc]
+//                                   initWithBarButtonSystemItem:UIBarButtonSystemItemEdit
+//                                   target:self
+//                                   action:@selector(editNotebook:)];
+    
+    // Toda tabla tiene un editButtonItem
+    self.navigationItem.leftBarButtonItem = self.editButtonItem;
+}
+
+
+#pragma mark - Actions
+
+- (void) addNewNotebook: (id) sender{
+    
+    // Creamos una nueva instancia de una libreta y Core Data se encarga de notificar al fetchResultsController, y este avisa a su delegado (el controlador AGTCoreDataTableViewController)
+    [FLGNotebook notebookWithName:@"Nueva libreta"
+                          context:self.fetchedResultsController.managedObjectContext];
+    // Todo objeto de Core Data sabe cual es su contexto, por eso se lo preguntamos a "self.fetchedResultsController"
+}
+
 
 @end
